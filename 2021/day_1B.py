c35a845ba994349e0684ce4a6b5c331617bb3197
Joe Kupresanin <28194478+kupresanin99@users.noreply.github.com>
2021-12-01 22:17:28 -0600

    Day 1 problem B solved

--- 2021/day_1B.py
+++ 2021/day_1B.py
@@ -1,10 +1,23 @@
 my_list = open('day_1.txt', 'r').read().split('\n')
+a = 0
+b = 0
+c = 0
+my_results = []
+my_counter = 0
+for reading in my_list:  # This loop collects sums of 3 elements into a list
+  my_counter += 1
+  a = b
+  b = c
+  c = int(reading)
+  if my_counter >= 3:
+    my_results.append(a + b + c)
+
 a = 100000  # arbitrary dummy large number
 b = 100000  # arbitrary dummy large number
 my_counter = 0
-for reading in my_list:
+for num in my_results:  # And this loop checks for increasing, element by element
   a = b
-  b = int(reading)
+  b = int(num)
   if b > a:
     my_counter += 1
 print(my_counter)
